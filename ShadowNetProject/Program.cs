06af99ae445e29ace8b135f3fcca5f2bd39af738
NewDelion <el.emena.crea@gmail.com>
2016-03-20 12:59:54 +0900

    add commands

--- ShadowNetProject/Program.cs
+++ ShadowNetProject/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ShadowNetProject
@@ -17,15 +18,52 @@ namespace ShadowNetProject
 
             while (true)
             {
-                string command = Console.ReadLine();
-                if (command == "quit")
+                string[] command = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (command.Length == 0)
+                    continue;
+                if (command[0] == "quit")
                 {
                     shadow.Stop();
                     break;
                 }
-                else if (command == "clear")
+                else if (command[0] == "clear")
                 {
                     Console.Clear();
+                }
+                else if (command[0] == "addserver" || command[0] == "adds")
+                {
+                    shadow.addServer(new ProxyLibrary.Server(command[1], int.Parse(command[2])));
+                    Console.WriteLine("サーバリストに{{{0}:{1}}}を追加しました。", command[1], command[2]);
+                }
+                else if (command[0] == "listserver" || command[0] == "ls")
+                {
+                    try
+                    {
+                        shadow.rwlock_ServerList.AcquireReaderLock(Timeout.Infinite);
+                        foreach (ProxyLibrary.Server server in shadow.ServerList)
+                            Console.WriteLine("{0}:{1}", server.ip, server.port);
+                    }
+                    finally
+                    {
+                        shadow.rwlock_ServerList.ReleaseLock();
+                    }
+                }
+                else if (command[0] == "listclient" || command[0] == "lc")
+                {
+                    try
+                    {
+                        shadow.rwlock_ClientList.AcquireReaderLock(Timeout.Infinite);
+                        foreach (ProxyLibrary.Client data in shadow.ClientList)
+                        {
+                            Console.WriteLine("Address: {0}:{1}", data.ip, data.port);
+                            Console.WriteLine(">> Server : {0}:{1}", data.ConnectingServer.ip, data.ConnectingServer.port);
+                            Console.WriteLine();
+                        }
+                    }
+                    finally
+                    {
+                        shadow.rwlock_ClientList.ReleaseLock();
+                    }
                 }
             }
 
